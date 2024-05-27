b3ba3f5da39cc6f0f2fb2d16d69fb8bbb8edb819
CopyCoder(VJ) <107760796+vanha777@users.noreply.github.com>
2024-05-27 23:50:00 +1000

    x

--- qside-next/src-tauri/src/lib.rs
+++ qside-next/src-tauri/src/lib.rs
@@ -1,5 +1,5 @@
 use scraper::{Html, Selector};
-// use reqwest;
+use reqwest;
 pub mod models;
 pub mod handler;
 // use crate::models;
@@ -8,22 +8,22 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-// #[tauri::command]
-// async fn scrap_web(r#type:String,url: String) -> crate::models::SocialMetaData {
-//     match r#type.as_str() {
-//         "Tiktok"=> crate::handler::tiktok_scrap(url).await,
-//         "Instagram" => crate::handler::instar_scrap(url).await,
-//         "Linkedin" => crate::handler::linkein_scrap(url).await,
-//         "Github" => crate::handler::instar_scrap(url).await,
-//         "My page" => crate::handler::instar_scrap(url).await,
-//         _=> crate::models::SocialMetaData::default()
-//     }
-// }
+#[tauri::command]
+async fn scrap_web(r#type:String,url: String) -> crate::models::SocialMetaData {
+    match r#type.as_str() {
+        "Tiktok"=> crate::handler::tiktok_scrap(url).await,
+        "Instagram" => crate::handler::instar_scrap(url).await,
+        "Linkedin" => crate::handler::linkein_scrap(url).await,
+        "Github" => crate::handler::instar_scrap(url).await,
+        "My page" => crate::handler::instar_scrap(url).await,
+        _=> crate::models::SocialMetaData::default()
+    }
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet,scrap_web])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
