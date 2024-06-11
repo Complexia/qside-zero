8f9f1a41ef24d06174455a0551835214102cc502
Complexia <complexia701@gmail.com>
2024-06-11 13:21:55 +1000

    connections page

--- qside-next/app/connections/page.tsx
+++ qside-next/app/connections/page.tsx
@@ -1,7 +1,27 @@
-const Connections = () => {
+"use client";
+import { Metadata } from "next"
+import Image from "next/image"
+import { Connections } from "@/components/connections/connections"
+
+import { useAuth } from "@/components/providers/authProvider";
+import { cn } from "@/utils/general/utils";
+
+
+  
+const ConnectionsPage = () => {
+    // @ts-ignore
+    const { public_user } = useAuth();
     return (
-        <div>
-            <h1>Connections</h1>
+        <div className="flex flex-col gap-20 items-center justify-center align-middle">
+            <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+                <main className="flex-1 flex flex-col lg:flex-row gap-6">
+                    <div className="flex flex-col lg:flex-row lg:space-x-4 space-y-4 lg:space-y-0">
+                        <Connections />
+                    </div>
+                </main>
+            </div>
         </div>
     )
 }
+
+export default ConnectionsPage;
---
+++ qside-next/components/connections/connections.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { ChevronDownIcon } from "@radix-ui/react-icons"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/registry/new-york/ui/avatar"
+import { Button } from "@/registry/new-york/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/registry/new-york/ui/card"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/registry/new-york/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/registry/new-york/ui/popover"
+
+export function Connections() {
+  return (
+    <div className="card w-96 glass">
+
+    
+    <Card className="border-0 text-primary">
+      <CardHeader>
+        <CardTitle>Connections</CardTitle>
+        <CardDescription>
+          Search box - search for connections
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-6">
+        <div className="flex items-center justify-between space-x-4">
+          <div className="flex items-center space-x-4">
+            <Avatar>
+              <AvatarImage src="/avatars/01.png" />
+              <AvatarFallback>OM</AvatarFallback>
+            </Avatar>
+            <div>
+              <p className="text-sm font-medium leading-none">Sofia Davis</p>
+              <p className="text-sm text-muted-foreground">description</p>
+            </div>
+          </div>
+          <button className="btn btn-primary ml-auto">
+            message
+          </button>
+        </div>
+        <div className="flex items-center justify-between space-x-4">
+          <div className="flex items-center space-x-4">
+            <Avatar>
+              <AvatarImage src="/avatars/02.png" />
+              <AvatarFallback>JL</AvatarFallback>
+            </Avatar>
+            <div>
+              <p className="text-sm font-medium leading-none">Jackson Lee</p>
+              <p className="text-sm text-muted-foreground">p@example.com</p>
+            </div>
+          </div>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                Member {" "}
+                <ChevronDownIcon className="ml-2 h-4 w-4 text-muted-foreground" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="p-0" align="end">
+              <Command>
+                <CommandInput placeholder="Select new role..." />
+                <CommandList>
+                  <CommandEmpty>No roles found.</CommandEmpty>
+                  <CommandGroup className="p-1.5">
+                    <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">
+                      <p>Viewer</p>
+                      <p className="text-sm text-muted-foreground">
+                        Can view and comment.
+                      </p>
+                    </CommandItem>
+                    <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">
+                      <p>Developer</p>
+                      <p className="text-sm text-muted-foreground">
+                        Can view, comment and edit.
+                      </p>
+                    </CommandItem>
+                    <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">
+                      <p>Billing</p>
+                      <p className="text-sm text-muted-foreground">
+                        Can view, comment and manage billing.
+                      </p>
+                    </CommandItem>
+                    <CommandItem className="teamaspace-y-1 flex flex-col items-start px-4 py-2">
+                      <p>Owner</p>
+                      <p className="text-sm text-muted-foreground">
+                        Admin-level access to all resources.
+                      </p>
+                    </CommandItem>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </CardContent>
+    </Card>
+    </div>
+  )
+}
